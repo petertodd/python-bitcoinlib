dca0aa939466794837c1e365dd71d5062af98c50
Jeff Garzik <jgarzik@exmulti.com>
2012-07-26 01:51:06 -0400

    testscript: count opcodes used in chain

--- testscript.py
+++ testscript.py
@@ -23,6 +23,29 @@ chaindb = ChainDb.ChainDb('/tmp/chaindb', log, mempool, NETWORKS['mainnet'])
 scanned = 0
 failures = 0
 warnings = 0
+opcount = {}
+
+def scan_script(scriptIn):
+	global opcount
+	script = CScript(scriptIn)
+	while script.pc < script.pend:
+		if not script.getop():
+			return False
+
+		sop = script.sop
+		if sop.op in OPCODE_NAMES:
+			name = OPCODE_NAMES[sop.op]
+		elif sop.op <= OP_PUSHDATA4:
+			name = 'OP_PUSHDATA'
+		else:
+			name = "0x%02x" % (sop.op,)
+
+		if name in opcount:
+			opcount[name] += 1
+		else:
+			opcount[name] = 1
+
+	return True
 
 for height in xrange(chaindb.getheight()):
 	blkhash = long(chaindb.height[str(height)])
@@ -32,9 +55,14 @@ for height in xrange(chaindb.getheight()):
 	block = CBlock()
 	block.deserialize(f)
 
-	if not block.is_valid():
-		log.write("Failed block #%d %064x (%d tx)" % (height, block.sha256, len(block.vtx)))
-		failures += 1
+	for tx in block.vtx:
+		if not tx.is_coinbase():
+			for txin in tx.vin:
+				if not scan_script(txin.scriptSig):
+					failures += 1
+		for txout in tx.vout:
+			if not scan_script(txout.scriptPubKey):
+				failures += 1
 
 	scanned += 1
 	if (scanned % 1000) == 0:
@@ -42,4 +70,7 @@ for height in xrange(chaindb.getheight()):
 
 
 print "Scanned %d blocks (%d warnings, %d failures)" % (scanned, warnings, failures)
+
+for k,v in opcount.iteritems():
+	print k, v
 
