fc0583d9c1f65e3dc6f6d635ca352bab62a4396d
Jeff Garzik <jgarzik@exmulti.com>
2012-07-28 00:17:07 -0400

    testscript: program now validates tx's in each block

--- testscript.py
+++ testscript.py
@@ -7,6 +7,7 @@
 #
 
 
+import sys
 import Log
 import MemPool
 import ChainDb
@@ -20,32 +21,22 @@ mempool = MemPool.MemPool(log)
 chaindb = ChainDb.ChainDb('/tmp/chaindb', log, mempool, NETWORKS['mainnet'])
 
 scanned = 0
+scanned_tx = 0
 failures = 0
-warnings = 0
 opcount = {}
 
-def scan_script(scriptIn):
-	global opcount
-	script = CScript(scriptIn)
-	while script.pc < script.pend:
-		if not script.getop():
+def scan_tx(tx):
+	tx.calc_sha256()
+	for i in xrange(len(tx.vin)):
+		txin = tx.vin[i]
+		txfrom = chaindb.gettx(txin.prevout.hash)
+		if not VerifySignature(txfrom, tx, i, 0):
+#			print "TX %064x/%d failed" % (tx.sha256, i)
+#			print "FROMTX", txfrom
+#			print "TOTX", tx
 			return False
-
-		sop = script.sop
-		if sop.op in OPCODE_NAMES:
-			name = OPCODE_NAMES[sop.op]
-		elif sop.op <= OP_PUSHDATA4:
-			name = 'OP_PUSHDATA'
-		else:
-			name = "0x%02x" % (sop.op,)
-
-		if name in opcount:
-			opcount[name] += 1
-		else:
-			opcount[name] = 1
-
 	return True
-
+	
 for height in xrange(chaindb.getheight()):
 	blkhash = long(chaindb.height[str(height)])
 	ser_hash = ser_uint256(blkhash)
@@ -55,20 +46,21 @@ for height in xrange(chaindb.getheight()):
 	block.deserialize(f)
 
 	for tx in block.vtx:
-		if not tx.is_coinbase():
-			for txin in tx.vin:
-				if not scan_script(txin.scriptSig):
-					failures += 1
-		for txout in tx.vout:
-			if not scan_script(txout.scriptPubKey):
-				failures += 1
+		if tx.is_coinbase():
+			continue
+
+		scanned_tx += 1
+
+		if not scan_tx(tx):
+			failures += 1
+#			sys.exit(1)
 
 	scanned += 1
 	if (scanned % 1000) == 0:
-		print "Scanned %d blocks (%d warnings, %d failures)" % (scanned, warnings, failures)
+		print "Scanned %d tx, %d blocks (%d failures)" % (scanned_tx, scanned, failures)
 
 
-print "Scanned %d blocks (%d warnings, %d failures)" % (scanned, warnings, failures)
+print "Scanned %d tx, %d blocks (%d failures)" % (scanned_tx, scanned, failures)
 
 for k,v in opcount.iteritems():
 	print k, v
