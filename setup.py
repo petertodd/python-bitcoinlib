4ca22010a89b8a5657b8756ea93ef4ccfad22a7c
Peter Todd <pete@petertodd.org>
2013-11-04 05:57:21 -0500

    Change setup.py version to 0.1
    Lets keep expectations reasonable...

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'README')) as f:
 requires = []
 
 setup(name='python-bitcoinlib',
-    version='1.0',
+    version='0.1',
     description='This python library provides an easy interface to the bitcoin data structures and protocol.',
     long_description=README,
     classifiers=[
