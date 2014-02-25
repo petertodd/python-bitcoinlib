dc13b1d7cc406efde3cba6ee9e0043232aa1fdd2
Peter Todd <pete@petertodd.org>
2014-02-25 05:20:08 -0500

    Put MurmurHash3 in bitcoin.bloom

--- bitcoin/bloom.py
+++ bitcoin/bloom.py
@@ -14,7 +14,71 @@ import math
 
 import bitcoin.core
 import bitcoin.core.serialize
-import bitcoin.hash
+
+def ROTL32(x, r):
+    assert x <= 0xFFFFFFFF
+    return ((x << r) & 0xFFFFFFFF) | (x >> (32 - r))
+
+def MurmurHash3(nHashSeed, vDataToHash):
+    """MurmurHash3 (x86_32)
+
+    Used for bloom filters. See http://code.google.com/p/smhasher/source/browse/trunk/MurmurHash3.cpp
+    """
+
+    assert nHashSeed <= 0xFFFFFFFF
+
+    h1 = nHashSeed
+    c1 = 0xcc9e2d51
+    c2 = 0x1b873593
+
+    # body
+    i = 0
+    while i < len(vDataToHash) - len(vDataToHash) % 4 \
+          and len(vDataToHash) - i >= 4:
+
+        k1 = struct.unpack(b"<L", vDataToHash[i:i+4])[0]
+
+        k1 = (k1 * c1) & 0xFFFFFFFF
+        k1 = ROTL32(k1, 15)
+        k1 = (k1 * c2) & 0xFFFFFFFF
+
+        h1 ^= k1
+        h1 = ROTL32(h1, 13)
+        h1 = (((h1*5) & 0xFFFFFFFF) + 0xe6546b64) & 0xFFFFFFFF
+
+        i += 4
+
+    # tail
+    k1 = 0
+    j = (len(vDataToHash) // 4) * 4
+    import sys
+    bord = ord
+    if sys.version > '3':
+        # In Py3 indexing bytes returns numbers, not characters
+        bord = lambda x: x
+    if len(vDataToHash) & 3 >= 3:
+        k1 ^= bord(vDataToHash[j+2]) << 16
+    if len(vDataToHash) & 3 >= 2:
+        k1 ^= bord(vDataToHash[j+1]) << 8
+    if len(vDataToHash) & 3 >= 1:
+        k1 ^= bord(vDataToHash[j])
+
+    k1 &= 0xFFFFFFFF
+    k1 = (k1 * c1) & 0xFFFFFFFF
+    k1 = ROTL32(k1, 15)
+    k1 = (k1 * c2) & 0xFFFFFFFF
+    h1 ^= k1
+
+    # finalization
+    h1 ^= len(vDataToHash) & 0xFFFFFFFF
+    h1 ^= (h1 & 0xFFFFFFFF) >> 16
+    h1 *= 0x85ebca6b
+    h1 ^= (h1 & 0xFFFFFFFF) >> 13
+    h1 *= 0xc2b2ae35
+    h1 ^= (h1 & 0xFFFFFFFF) >> 16
+
+    return h1 & 0xFFFFFFFF
+
 
 class CBloomFilter(bitcoin.core.serialize.Serializable):
     # 20,000 items with fp rate < 0.1% or 10,000 items and <0.0001%
@@ -51,7 +115,7 @@ class CBloomFilter(bitcoin.core.serialize.Serializable):
         self.nFlags = nFlags
 
     def bloom_hash(self, nHashNum, vDataToHash):
-        return bitcoin.hash.MurmurHash3(((nHashNum * 0xFBA4C795) + self.nTweak) & 0xFFFFFFFF, vDataToHash) % (len(self.vData) * 8)
+        return MurmurHash3(((nHashNum * 0xFBA4C795) + self.nTweak) & 0xFFFFFFFF, vDataToHash) % (len(self.vData) * 8)
 
     __bit_mask = bytearray([0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80])
     def insert(self, elem):
--- bitcoin/tests/test_bloom.py
+++ bitcoin/tests/test_bloom.py
@@ -12,6 +12,29 @@ from binascii import unhexlify
 import bitcoin.core
 from bitcoin.bloom import *
 
+class Test_MurmurHash3(unittest.TestCase):
+    def test(self):
+        def T(expected, seed, data):
+            self.assertEqual(MurmurHash3(seed, unhexlify(data)), expected)
+
+        T(0x00000000, 0x00000000, b"");
+        T(0x6a396f08, 0xFBA4C795, b"");
+        T(0x81f16f39, 0xffffffff, b"");
+
+        T(0x514e28b7, 0x00000000, b"00");
+        T(0xea3f0b17, 0xFBA4C795, b"00");
+        T(0xfd6cf10d, 0x00000000, b"ff");
+
+        T(0x16c6b7ab, 0x00000000, b"0011");
+        T(0x8eb51c3d, 0x00000000, b"001122");
+        T(0xb4471bf8, 0x00000000, b"00112233");
+        T(0xe2301fa8, 0x00000000, b"0011223344");
+        T(0xfc2e4a15, 0x00000000, b"001122334455");
+        T(0xb074502c, 0x00000000, b"00112233445566");
+        T(0x8034d2a0, 0x00000000, b"0011223344556677");
+        T(0xb4698def, 0x00000000, b"001122334455667788");
+
+
 class Test_CBloomFilter(unittest.TestCase):
     def test_create_insert_serialize(self):
         filter = CBloomFilter(3, 0.01, 0, CBloomFilter.UPDATE_ALL)
