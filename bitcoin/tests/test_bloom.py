5b84ae504b15ecc838565691c89940e38e360372
Peter Todd <pete@petertodd.org>
2014-07-23 19:32:38 -0400

    Replace unhexlify() with x() in tests

--- bitcoin/tests/test_bloom.py
+++ bitcoin/tests/test_bloom.py
@@ -7,32 +7,31 @@ import json
 import os
 import unittest
 
-from binascii import unhexlify
-
 import bitcoin.core
+from bitcoin.core import x
 from bitcoin.bloom import *
 
 class Test_MurmurHash3(unittest.TestCase):
     def test(self):
         def T(expected, seed, data):
-            self.assertEqual(MurmurHash3(seed, unhexlify(data)), expected)
+            self.assertEqual(MurmurHash3(seed, x(data)), expected)
 
-        T(0x00000000, 0x00000000, b"");
-        T(0x6a396f08, 0xFBA4C795, b"");
-        T(0x81f16f39, 0xffffffff, b"");
+        T(0x00000000, 0x00000000, "");
+        T(0x6a396f08, 0xFBA4C795, "");
+        T(0x81f16f39, 0xffffffff, "");
 
-        T(0x514e28b7, 0x00000000, b"00");
-        T(0xea3f0b17, 0xFBA4C795, b"00");
-        T(0xfd6cf10d, 0x00000000, b"ff");
+        T(0x514e28b7, 0x00000000, "00");
+        T(0xea3f0b17, 0xFBA4C795, "00");
+        T(0xfd6cf10d, 0x00000000, "ff");
 
-        T(0x16c6b7ab, 0x00000000, b"0011");
-        T(0x8eb51c3d, 0x00000000, b"001122");
-        T(0xb4471bf8, 0x00000000, b"00112233");
-        T(0xe2301fa8, 0x00000000, b"0011223344");
-        T(0xfc2e4a15, 0x00000000, b"001122334455");
-        T(0xb074502c, 0x00000000, b"00112233445566");
-        T(0x8034d2a0, 0x00000000, b"0011223344556677");
-        T(0xb4698def, 0x00000000, b"001122334455667788");
+        T(0x16c6b7ab, 0x00000000, "0011");
+        T(0x8eb51c3d, 0x00000000, "001122");
+        T(0xb4471bf8, 0x00000000, "00112233");
+        T(0xe2301fa8, 0x00000000, "0011223344");
+        T(0xfc2e4a15, 0x00000000, "001122334455");
+        T(0xb074502c, 0x00000000, "00112233445566");
+        T(0x8034d2a0, 0x00000000, "0011223344556677");
+        T(0xb4698def, 0x00000000, "001122334455667788");
 
 
 class Test_CBloomFilter(unittest.TestCase):
@@ -41,21 +40,21 @@ class Test_CBloomFilter(unittest.TestCase):
 
         def T(elem):
             """Filter contains elem"""
-            elem = unhexlify(elem)
+            elem = x(elem)
             filter.insert(elem)
             self.assertTrue(filter.contains(elem))
 
         def F(elem):
             """Filter does not contain elem"""
-            elem = unhexlify(elem)
+            elem = x(elem)
             self.assertFalse(filter.contains(elem))
 
-        T(b'99108ad8ed9bb6274d3980bab5a85c048f0950c8')
-        F(b'19108ad8ed9bb6274d3980bab5a85c048f0950c8')
-        T(b'b5a2c786d9ef4658287ced5914b37a1b4aa32eee')
-        T(b'b9300670b4c5366e95b2699e8b18bc75e5f729c5')
+        T('99108ad8ed9bb6274d3980bab5a85c048f0950c8')
+        F('19108ad8ed9bb6274d3980bab5a85c048f0950c8')
+        T('b5a2c786d9ef4658287ced5914b37a1b4aa32eee')
+        T('b9300670b4c5366e95b2699e8b18bc75e5f729c5')
 
-        self.assertEqual(filter.serialize(), unhexlify(b'03614e9b050000000000000001'))
+        self.assertEqual(filter.serialize(), x('03614e9b050000000000000001'))
 
     def test_create_insert_serialize_with_tweak(self):
         # Same test as bloom_create_insert_serialize, but we add a nTweak of 100
@@ -63,29 +62,29 @@ class Test_CBloomFilter(unittest.TestCase):
 
         def T(elem):
             """Filter contains elem"""
-            elem = unhexlify(elem)
+            elem = x(elem)
             filter.insert(elem)
             self.assertTrue(filter.contains(elem))
 
         def F(elem):
             """Filter does not contain elem"""
-            elem = unhexlify(elem)
+            elem = x(elem)
             self.assertFalse(filter.contains(elem))
 
-        T(b'99108ad8ed9bb6274d3980bab5a85c048f0950c8')
-        F(b'19108ad8ed9bb6274d3980bab5a85c048f0950c8')
-        T(b'b5a2c786d9ef4658287ced5914b37a1b4aa32eee')
-        T(b'b9300670b4c5366e95b2699e8b18bc75e5f729c5')
+        T('99108ad8ed9bb6274d3980bab5a85c048f0950c8')
+        F('19108ad8ed9bb6274d3980bab5a85c048f0950c8')
+        T('b5a2c786d9ef4658287ced5914b37a1b4aa32eee')
+        T('b9300670b4c5366e95b2699e8b18bc75e5f729c5')
 
-        self.assertEqual(filter.serialize(), unhexlify(b'03ce4299050000000100008001'))
+        self.assertEqual(filter.serialize(), x('03ce4299050000000100008001'))
 
     def test_bloom_create_insert_key(self):
         filter = CBloomFilter(2, 0.001, 0, CBloomFilter.UPDATE_ALL)
 
-        pubkey = unhexlify(b'045B81F0017E2091E2EDCD5EECF10D5BDD120A5514CB3EE65B8447EC18BFC4575C6D5BF415E54E03B1067934A0F0BA76B01C6B9AB227142EE1D543764B69D901E0')
+        pubkey = x('045B81F0017E2091E2EDCD5EECF10D5BDD120A5514CB3EE65B8447EC18BFC4575C6D5BF415E54E03B1067934A0F0BA76B01C6B9AB227142EE1D543764B69D901E0')
         pubkeyhash = bitcoin.core.Hash160(pubkey)
 
         filter.insert(pubkey)
         filter.insert(pubkeyhash)
 
-        self.assertEqual(filter.serialize(), unhexlify(b'038fc16b080000000000000001'))
+        self.assertEqual(filter.serialize(), x('038fc16b080000000000000001'))
--- bitcoin/tests/test_script.py
+++ bitcoin/tests/test_script.py
@@ -6,29 +6,27 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import unittest
 import os
 
-from binascii import unhexlify
-
 from bitcoin.core import b2x,x
 from bitcoin.core.script import *
 
 class Test_CScriptOp(unittest.TestCase):
     def test_pushdata(self):
         def T(data, expected):
-            data = unhexlify(data)
-            expected = unhexlify(expected)
+            data = x(data)
+            expected = x(expected)
             serialized_data = CScriptOp.encode_op_pushdata(data)
             self.assertEqual(serialized_data, expected)
 
-        T(b'', b'00')
-        T(b'00', b'0100')
-        T(b'0011223344556677', b'080011223344556677')
-        T(b'ff'*0x4b, b'4b' + b'ff'*0x4b)
-        T(b'ff'*0x4c, b'4c4c' + b'ff'*0x4c)
-        T(b'ff'*0x4c, b'4c4c' + b'ff'*0x4c)
-        T(b'ff'*0xff, b'4cff' + b'ff'*0xff)
-        T(b'ff'*0x100, b'4d0001' + b'ff'*0x100)
-        T(b'ff'*0xffff, b'4dffff' + b'ff'*0xffff)
-        T(b'ff'*0x10000, b'4e00000100' + b'ff'*0x10000)
+        T('', '00')
+        T('00', '0100')
+        T('0011223344556677', '080011223344556677')
+        T('ff'*0x4b, '4b' + 'ff'*0x4b)
+        T('ff'*0x4c, '4c4c' + 'ff'*0x4c)
+        T('ff'*0x4c, '4c4c' + 'ff'*0x4c)
+        T('ff'*0xff, '4cff' + 'ff'*0xff)
+        T('ff'*0x100, '4d0001' + 'ff'*0x100)
+        T('ff'*0xffff, '4dffff' + 'ff'*0xffff)
+        T('ff'*0x10000, '4e00000100' + 'ff'*0x10000)
 
     def test_is_singleton(self):
         self.assertTrue(OP_0 is CScriptOp(0x00))
@@ -77,7 +75,7 @@ class Test_CScriptOp(unittest.TestCase):
 class Test_CScript(unittest.TestCase):
     def test_tokenize_roundtrip(self):
         def T(serialized_script, expected_tokens, test_roundtrip=True):
-            serialized_script = unhexlify(serialized_script)
+            serialized_script = x(serialized_script)
             script_obj = CScript(serialized_script)
             actual_tokens = list(script_obj)
             self.assertEqual(actual_tokens, expected_tokens)
@@ -86,94 +84,94 @@ class Test_CScript(unittest.TestCase):
                 recreated_script = CScript(actual_tokens)
                 self.assertEqual(recreated_script, serialized_script)
 
-        T(b'', [])
+        T('', [])
 
         # standard pushdata
-        T(b'00', [b''])
-        T(b'0100', [b'\x00'])
-        T(b'4b' + b'ff'*0x4b, [b'\xff'*0x4b])
+        T('00', [b''])
+        T('0100', [b'\x00'])
+        T('4b' + 'ff'*0x4b, [b'\xff'*0x4b])
 
         # non-optimal pushdata
-        T(b'4c00', [b''], False)
-        T(b'4c04deadbeef', [unhexlify(b'deadbeef')], False)
-        T(b'4d0000', [b''], False)
-        T(b'4d0400deadbeef', [unhexlify(b'deadbeef')], False)
-        T(b'4e00000000', [b''], False)
-        T(b'4e04000000deadbeef', [unhexlify(b'deadbeef')], False)
+        T('4c00', [b''], False)
+        T('4c04deadbeef', [x('deadbeef')], False)
+        T('4d0000', [b''], False)
+        T('4d0400deadbeef', [x('deadbeef')], False)
+        T('4e00000000', [b''], False)
+        T('4e04000000deadbeef', [x('deadbeef')], False)
 
         # numbers
-        T(b'4f', [OP_1NEGATE])
-        T(b'51', [0x1])
-        T(b'52', [0x2])
-        T(b'53', [0x3])
-        T(b'54', [0x4])
-        T(b'55', [0x5])
-        T(b'56', [0x6])
-        T(b'57', [0x7])
-        T(b'58', [0x8])
-        T(b'59', [0x9])
-        T(b'5a', [0xa])
-        T(b'5b', [0xb])
-        T(b'5c', [0xc])
-        T(b'5d', [0xd])
-        T(b'5e', [0xe])
-        T(b'5f', [0xf])
+        T('4f', [OP_1NEGATE])
+        T('51', [0x1])
+        T('52', [0x2])
+        T('53', [0x3])
+        T('54', [0x4])
+        T('55', [0x5])
+        T('56', [0x6])
+        T('57', [0x7])
+        T('58', [0x8])
+        T('59', [0x9])
+        T('5a', [0xa])
+        T('5b', [0xb])
+        T('5c', [0xc])
+        T('5d', [0xd])
+        T('5e', [0xe])
+        T('5f', [0xf])
 
         # some opcodes
-        T(b'9b', [OP_BOOLOR])
-        T(b'9a9b', [OP_BOOLAND, OP_BOOLOR])
-        T(b'ff', [OP_INVALIDOPCODE])
-        T(b'fafbfcfd', [CScriptOp(0xfa), CScriptOp(0xfb), CScriptOp(0xfc), CScriptOp(0xfd)])
+        T('9b', [OP_BOOLOR])
+        T('9a9b', [OP_BOOLAND, OP_BOOLOR])
+        T('ff', [OP_INVALIDOPCODE])
+        T('fafbfcfd', [CScriptOp(0xfa), CScriptOp(0xfb), CScriptOp(0xfc), CScriptOp(0xfd)])
 
         # all three types
-        T(b'512103e2a0e6a91fa985ce4dda7f048fca5ec8264292aed9290594321aa53d37fdea32410478d430274f8c5ec1321338151e9f27f4c676a008bdf8638d07c0b6be9ab35c71a1518063243acd4dfe96b66e3f2ec8013c8e072cd09b3834a19f81f659cc345552ae',
+        T('512103e2a0e6a91fa985ce4dda7f048fca5ec8264292aed9290594321aa53d37fdea32410478d430274f8c5ec1321338151e9f27f4c676a008bdf8638d07c0b6be9ab35c71a1518063243acd4dfe96b66e3f2ec8013c8e072cd09b3834a19f81f659cc345552ae',
           [1,
-           unhexlify(b'03e2a0e6a91fa985ce4dda7f048fca5ec8264292aed9290594321aa53d37fdea32'),
-           unhexlify(b'0478d430274f8c5ec1321338151e9f27f4c676a008bdf8638d07c0b6be9ab35c71a1518063243acd4dfe96b66e3f2ec8013c8e072cd09b3834a19f81f659cc3455'),
+           x('03e2a0e6a91fa985ce4dda7f048fca5ec8264292aed9290594321aa53d37fdea32'),
+           x('0478d430274f8c5ec1321338151e9f27f4c676a008bdf8638d07c0b6be9ab35c71a1518063243acd4dfe96b66e3f2ec8013c8e072cd09b3834a19f81f659cc3455'),
            2,
            OP_CHECKMULTISIG])
 
     def test_invalid_scripts(self):
         def T(serialized):
             with self.assertRaises(CScriptInvalidError):
-                list(CScript(unhexlify(serialized)))
+                list(CScript(x(serialized)))
 
-        T(b'01')
-        T(b'02')
-        T(b'0201')
-        T(b'4b')
-        T(b'4b' + b'ff'*0x4a)
-        T(b'4c')
-        T(b'4cff' + b'ff'*0xfe)
-        T(b'4d')
-        T(b'4dff')
-        T(b'4dffff' + b'ff'*0xfffe)
-        T(b'4e')
-        T(b'4effffff')
-        T(b'4effffffff' + b'ff'*0xfffe) # not going to test with 4GiB-1...
+        T('01')
+        T('02')
+        T('0201')
+        T('4b')
+        T('4b' + 'ff'*0x4a)
+        T('4c')
+        T('4cff' + 'ff'*0xfe)
+        T('4d')
+        T('4dff')
+        T('4dffff' + 'ff'*0xfffe)
+        T('4e')
+        T('4effffff')
+        T('4effffffff' + 'ff'*0xfffe) # not going to test with 4GiB-1...
 
     def test_equality(self):
         # Equality is on the serialized script, not the logical meaning.
         # This is important for P2SH.
         def T(serialized1, serialized2, are_equal):
-            script1 = CScript(unhexlify(serialized1))
-            script2 = CScript(unhexlify(serialized2))
+            script1 = CScript(x(serialized1))
+            script2 = CScript(x(serialized2))
             if are_equal:
                 self.assertEqual(script1, script2)
             else:
                 self.assertNotEqual(script1, script2)
 
-        T(b'', b'', True)
-        T(b'', b'00', False)
-        T(b'00', b'00', True)
-        T(b'00', b'01', False)
-        T(b'01ff', b'01ff', True)
-        T(b'fc01ff', b'01ff', False)
+        T('', '', True)
+        T('', '00', False)
+        T('00', '00', True)
+        T('00', '01', False)
+        T('01ff', '01ff', True)
+        T('fc01ff', '01ff', False)
 
         # testing equality on an invalid script is legal, and evaluates based
         # on the serialization
-        T(b'4e', b'4e', True)
-        T(b'4e', b'4e00', False)
+        T('4e', '4e', True)
+        T('4e', '4e00', False)
 
     def test_add(self):
         script = CScript()
@@ -233,92 +231,92 @@ class Test_CScript(unittest.TestCase):
         T( CScript([1, x('7ac977d8373df875eceda362298e5d09d4b72b53'), OP_DROP]),
           "CScript([1, x('7ac977d8373df875eceda362298e5d09d4b72b53'), OP_DROP])")
 
-        T(CScript(unhexlify(b'0001ff515261ff')),
+        T(CScript(x('0001ff515261ff')),
           "CScript([x(''), x('ff'), 1, 2, OP_NOP, OP_INVALIDOPCODE])")
 
         # truncated scripts
-        T(CScript(unhexlify(b'6101')),
+        T(CScript(x('6101')),
           "CScript([OP_NOP, x('')...<ERROR: PUSHDATA(1): truncated data>])")
 
-        T(CScript(unhexlify(b'614bff')),
+        T(CScript(x('614bff')),
           "CScript([OP_NOP, x('ff')...<ERROR: PUSHDATA(75): truncated data>])")
 
-        T(CScript(unhexlify(b'614c')),
+        T(CScript(x('614c')),
           "CScript([OP_NOP, <ERROR: PUSHDATA1: missing data length>])")
 
-        T(CScript(unhexlify(b'614c0200')),
+        T(CScript(x('614c0200')),
           "CScript([OP_NOP, x('00')...<ERROR: PUSHDATA1: truncated data>])")
 
     def test_is_p2sh(self):
         def T(serialized, b):
-            script = CScript(unhexlify(serialized))
+            script = CScript(x(serialized))
             self.assertEqual(script.is_p2sh(), b)
 
         # standard P2SH
-        T(b'a9146567e91196c49e1dffd09d5759f6bbc0c6d4c2e587', True)
+        T('a9146567e91196c49e1dffd09d5759f6bbc0c6d4c2e587', True)
 
         # NOT a P2SH txout due to the non-optimal PUSHDATA encoding
-        T(b'a94c146567e91196c49e1dffd09d5759f6bbc0c6d4c2e587', False)
+        T('a94c146567e91196c49e1dffd09d5759f6bbc0c6d4c2e587', False)
 
     def test_is_push_only(self):
         def T(serialized, b):
-            script = CScript(unhexlify(serialized))
+            script = CScript(x(serialized))
             self.assertEqual(script.is_push_only(), b)
 
-        T(b'', True)
-        T(b'00', True)
-        T(b'0101', True)
-        T(b'4c00', True)
-        T(b'4d0000', True)
-        T(b'4e00000000', True)
-        T(b'4f', True)
+        T('', True)
+        T('00', True)
+        T('0101', True)
+        T('4c00', True)
+        T('4d0000', True)
+        T('4e00000000', True)
+        T('4f', True)
 
         # OP_RESERVED *is* considered to be a pushdata op by is_push_only!
         # Or specifically, the IsPushOnly() used in P2SH validation.
-        T(b'50', True)
+        T('50', True)
 
-        T(b'51', True)
-        T(b'52', True)
-        T(b'53', True)
-        T(b'54', True)
-        T(b'55', True)
-        T(b'56', True)
-        T(b'57', True)
-        T(b'58', True)
-        T(b'59', True)
-        T(b'5a', True)
-        T(b'5b', True)
-        T(b'5c', True)
-        T(b'5d', True)
-        T(b'5e', True)
-        T(b'5f', True)
-        T(b'60', True)
+        T('51', True)
+        T('52', True)
+        T('53', True)
+        T('54', True)
+        T('55', True)
+        T('56', True)
+        T('57', True)
+        T('58', True)
+        T('59', True)
+        T('5a', True)
+        T('5b', True)
+        T('5c', True)
+        T('5d', True)
+        T('5e', True)
+        T('5f', True)
+        T('60', True)
 
-        T(b'61', False)
+        T('61', False)
 
     def test_is_unspendable(self):
         def T(serialized, b):
-            script = CScript(unhexlify(serialized))
+            script = CScript(x(serialized))
             self.assertEqual(script.is_unspendable(), b)
 
-        T(b'', False)
-        T(b'00', False)
-        T(b'006a', False)
-        T(b'6a', True)
-        T(b'6a6a', True)
-        T(b'6a51', True)
+        T('', False)
+        T('00', False)
+        T('006a', False)
+        T('6a', True)
+        T('6a6a', True)
+        T('6a51', True)
 
     def test_is_valid(self):
         def T(serialized, b):
-            script = CScript(unhexlify(serialized))
+            script = CScript(x(serialized))
             self.assertEqual(script.is_valid(), b)
 
-        T(b'', True)
-        T(b'00', True)
-        T(b'01', False)
+        T('', True)
+        T('00', True)
+        T('01', False)
 
         # invalid opcodes do not by themselves make a script invalid
-        T(b'ff', True)
+        T('ff', True)
 
     def test_to_p2sh_scriptPubKey(self):
         def T(redeemScript, expected_hex_bytes):
