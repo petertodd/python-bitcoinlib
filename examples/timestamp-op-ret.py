df1adeb1d0ed3f841c5028799b9c47bf0f8ee070
Peter Todd <pete@petertodd.org>
2014-12-22 10:13:37 -0500

    Fix v0.3.0 incompatibility

--- examples/timestamp-op-ret.py
+++ examples/timestamp-op-ret.py
@@ -55,7 +55,7 @@ for digest in digests:
     change_pubkey = proxy.validateaddress(change_addr)['pubkey']
     change_out = CMutableTxOut(MAX_MONEY, CScript([change_pubkey, OP_CHECKSIG]))
 
-    digest_outs = [CMutableTxOut(0, CScript([script.OP_RETURN, digest]))]
+    digest_outs = [CMutableTxOut(0, CScript([OP_RETURN, digest]))]
 
     txouts = [change_out] + digest_outs
 
