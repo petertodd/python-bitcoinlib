6f163193c6461f958a5954386c697547de039b88
benjyz <benjamin.l.cordes@gmail.com>
2015-10-28 19:10:35 +0100

    formating

--- examples/difficulty.py
+++ examples/difficulty.py
@@ -1,24 +1,29 @@
+#!/usr/bin/env python3
+
 """
-print difficult of blocks through time
+simple example: print difficulty of blocks through time
 """
 
 import bitcoin.rpc
 import datetime
 
-proxy = bitcoin.rpc.Proxy()
-
 def datestr(utime):
     dateFormat = '%Y-%m-%d'
     return datetime.datetime.fromtimestamp(int(utime)).strftime(dateFormat)
 
-n = 100
-start = 1
-n = 380965 #last block as of 2015-10-27
-blocks_day = 144
-skip = blocks_day*30*3 # every 3 months
-print('*** Bitcoin difficulty ***')
-for i in range(start, start+n,skip):
-    h = proxy.getblockhash(i)
-    block = proxy.getblock(h)
+def printDifficulty():
+    proxy = bitcoin.rpc.Proxy()
+    n = 100
+    start = 1
+    n = 380965 #last block as of 2015-10-27
+    blocks_day = 144
+    skip = blocks_day*30*3 # every 3 months
+    print('*** Bitcoin difficulty ***')
+    for i in range(start, start+n,skip):
+        h = proxy.getblockhash(i)
+        block = proxy.getblock(h)
 
-    print('%s %s'%(datestr(block.nTime),block.difficulty))
+        print('%s %s'%(datestr(block.nTime),block.difficulty))
+
+if __name__=='__main__':
+    printDifficulty()
