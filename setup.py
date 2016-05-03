1de70182a62160fc9a1cd8993f011170de8c13f8
jadeblaquiere <jadeblaquiere@yahoo.com>
2016-05-02 21:56:17 -0400

    fixes for name change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 import os
 
-from bitcoin import __version__
+from ctcoin import __version__
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README')) as f:
@@ -11,18 +11,18 @@ with open(os.path.join(here, 'README')) as f:
 
 requires = []
 
-setup(name='python-bitcoinlib',
+setup(name='python-ctcoinlib',
       version=__version__,
-      description='The Swiss Army Knife of the Bitcoin protocol.',
+      description='The Swiss Army Knife of the CTCoin protocol.',
       long_description=README,
       classifiers=[
           "Programming Language :: Python",
           "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
       ],
-      url='https://github.com/petertodd/python-bitcoinlib',
-      keywords='bitcoin',
+      url='https://github.com/jadeblaquiere/python-ctcoinlib',
+      keywords='ctcoin',
       packages=find_packages(),
       zip_safe=False,
       install_requires=requires,
-      test_suite="bitcoin.tests"
+      test_suite="ctcoin.tests"
      )
